ba5ebd3d43c6567aaa8d3fc40e812e83d8cb8de6
MeckeDev <crafted.mecke@gmail.com>
2024-11-12 15:00:17 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
+      pages: '_app',
+      assets: '_app',
       fallback: 'index.html',  // Fallback for SPA mode
       strict: false
-    })
+    }),
+    paths: {
+      base: '/pin_collection',  // Change this to match your repo name
+    },
   }
 };
